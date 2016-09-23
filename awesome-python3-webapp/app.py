8b9de9df693e64a12cd20c64bcce41f4179f9735
dongjian06 <dongjian06@baidu.com>
2016-09-23 10:24:31 +0800

    add new code

---
+++ awesome-python3-webapp/app.py
@@ -0,0 +1,82 @@
+# import logging; logging.basicConfig(level=logging.INFO)
+#
+# import asyncio, os, json, time
+# from datetime import datetime
+#
+#
+# from aiohttp import web
+#
+# def index(request):
+#     return web.Response(body=b'<h1>Awesome</h1>')
+#
+# @asyncio.coroutine
+# def init(loop):
+#     app = web.Application(loop=loop)
+#     app.router.add_route('GET','/',index)
+#     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+#     logging.info('server started at http://127.0.0.1:9000....')
+#     return srv
+#
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(init(loop))
+# loop.run_forever()
+
+
+# def consumer():
+#     r = ''
+#     while True:
+#         n = yield r
+#         if not n:
+#             return
+#         print('[CONSUMER] Consuming %s...' % n)
+#         r = '200 OK'
+#
+# def produce(c):
+#     c.send(None)
+#     n = 0
+#     while n < 5:
+#         n = n + 1
+#         print('[PRODUCER] Producing %s...' % n)
+#         r = c.send(n)
+#         print('[PRODUCER] Consumer return: %s' % r)
+#     c.close()
+#
+# c = consumer()
+# produce(c)
+#
+# import asyncio
+#
+# @asyncio.coroutine
+# def wget(host):
+#     print('wget %s...' % host)
+#     connect = asyncio.open_connection(host, 80)
+#     reader, writer = yield from connect
+#     header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
+#     writer.write(header.encode('utf-8'))
+#     yield from writer.drain()
+#     while True:
+#         line = yield from reader.readline()
+#         if line == b'\r\n':
+#             break
+#         print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
+#     # Ignore the body, close the socket
+#     writer.close()
+#
+# loop = asyncio.get_event_loop()
+# tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
+# loop.run_until_complete(asyncio.wait(tasks))
+# loop.close()
+
+import threading
+import asyncio
+
+@asyncio.coroutine
+def hello():
+    print('Hello world! (%s)' % threading.currentThread())
+    yield from asyncio.sleep(1)
+    print('Hello again! (%s)' % threading.currentThread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(), hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
